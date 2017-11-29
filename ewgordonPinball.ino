5577d38c1a626bfce650b989a3e4a171f685c145
Evan Gordon <evanwgordon@yahoo.com>
2017-11-28 16:10:40 -0800

    add credit, add player, light number of players lights, and light active player bulb

--- ewgordonPinball.ino
+++ ewgordonPinball.ino
@@ -1,12 +1,17 @@
 #include <BallyLib.h>
-//audio:
-/*
-# Trigger mode (0..3)
-  #  0:Level triggered. Repeats. Responds to code change immediately.
-  #  1:Level triggered (sustained).  Repeats. Always plays to end of file.
-  #  2:Edge triggered.  Plays once. Ignores code changes during playback.
-  #  3:Edge triggered (retriggerable).  Plays once.  Responds to code change.
-*/
+
+
+#define CR_ROW   0
+#define CR_COL   5
+#define COIN_ROW 1
+#define COIN_COL 0
+#define PLAYER_UP_ROW 14
+#define NUM_PLAYERS_ROW 13
+Bally bally;
+
+int activePlayer = 0;
+int LMH[3] = {2,8,4} ; // {8,4,12} ; 
+
 void setup() {
   Serial.begin(9600);
   //displays, set any lamps that should always be on
@@ -14,16 +19,14 @@ void setup() {
 
 void loop() {
   bool gameStarted = false;
-  numPlayers = 0;
+  int numPlayers = 0, credits = 0;
   long int scores[4] = {0};
-  int scoreFound = waitPlayers();//accept players until game starts
-  scores[0] = scoreFound;
+  //int scoreFound = waitPlayers(numPlayers, credits);//accept players until game starts
+  //scores[0] = scoreFound;
   //init S/W state: scores, player number, ball number, drop target 
   //counters, any other game and/or ball state variables
   //init H/W, such as game over lamp
-  int button = bally.waitForTestCreditCoun(CR_ROW, CR_COL, COIN_ROW, COIN_COL);
-  if(button == CREDIT){
-    gameOver = false;
+  int r = waitPlayers(numPlayers, credits);
     //turn off game over, indicate #players=1, set player1 score to zero
     //loop for each player and ball (3 balls per player per game)
       //zero the switch memory so don’t retain sticky hits from before
@@ -38,6 +41,116 @@ void loop() {
     //until each player has played 3 balls
     //check for high score (optional)
     //perform random score match (optional), fire knocker on match
-  }
 
 }
+
+
+bool creditReleased()
+{
+  static bool values[3] = {false};
+  for(int i = 0; i < 3; i++)
+  {
+    values[0] = values[1];
+    values[1] = values[2];
+    values[2] = bally.getCabSwitch(1, 0);
+    if(!values[0] && values[1] && values[2])
+     {
+      return true;
+     }
+  }
+  return false; 
+}
+
+bool addPlayerReleased()
+{
+  static bool values[3] = {false};
+  for(int i = 0; i < 3; i++)
+  {
+    values[0] = values[1];
+    values[1] = values[2];
+    values[2] = bally.getCabSwitch(0, 5);
+    if(!values[0] && values[1] && values[2])
+     {
+      return true;
+     }
+  }
+  return false; 
+}
+
+//return score of element ball hit?
+int waitPlayers(int& numPlayers, int& credits)
+{
+  while(numPlayers < 4/*wait for game start condition*/){
+    bool result = creditReleased();
+    if(result){
+      if(credits < 4){//are more than 4 credits allowed?
+        credits++;
+        bally.setDisplay(5, 0, (char)credits);//this last argument might need to be converted to BCD
+      }
+    }
+    result = addPlayerReleased();
+    if(result){
+      if(credits > 0 && numPlayers < 4){
+        switchPlayer(0);//set active player 0
+        lightLamp(NUM_PLAYERS_ROW, numPlayers);
+        credits--;
+        numPlayers++;
+        bally.fireSolenoid(6, true);//eject ball
+      }
+    }
+  }
+}
+
+void lightLamp(int row, int col)
+{
+  for(int i = 0; i < 4; ++i)
+  {
+    int a = false;
+    if(i == col){a = true;}
+    bally.setLamp( row, i, a);
+  }
+}
+
+void switchPlayer(const int& playerIndex)
+{
+  //switch activePlayer logic
+  if(0 <= playerIndex && playerIndex <= 3)
+  {
+    activePlayer = playerIndex;
+    Serial.println("TODO switch active player score");
+    //bally.setDisplay( , );//set score display
+    lightLamp(PLAYER_UP_ROW, activePlayer);//set player number display
+  }
+}
+
+void detectBallAction(int& activePlayer, long int scores[])//will need to change player number if ball falls in hole
+{
+  //scan all switches in machine for ball action,
+}
+
+void setScore(int dispNumber, long int score)
+{
+  long int scoreCpy = score;
+  for(int i = 0; i < N_DIGITS; ++i)
+    {
+      int temp = removeSmallestDigit(score);
+      if(temp == 0)
+      {
+        temp = -1;
+      }
+      Serial.println("TODO set display code here");
+      //bally.setDisplay( , );
+    } 
+  //make leading zeros -1?
+}
+
+int removeSmallestDigit(long int& score)//takes score splits off leading bit from original score
+{
+  int result = (int)(score % 10);
+  score = score / 10;
+  return result;
+}
+
+
+
+
