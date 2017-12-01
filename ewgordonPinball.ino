10613286f9a0e3cb27090d98dedd836566b617a3
Evan Gordon <evanwgordon@yahoo.com>
2017-12-01 12:54:26 -0800

    added first draft of playfield logic, fixed turn++ loop

--- ewgordonPinball.ino
+++ ewgordonPinball.ino
@@ -16,7 +16,6 @@ void setup() {
 
 void loop() {
   ballLive = false;
-  scores[4] = {0};
   activePlayer = -1;
   int numPlayers = -1, credits = 0, turn = 0;
   initScreen();//init H/W, such as game over lamp
@@ -36,12 +35,16 @@ void loop() {
         //init any S/W and H/W state that should reset on each ball
         //light current player up and display the ball number
         //bally.fireSolenoid(6, false);//fire the outhole solenoid to eject a ball
-        bool turnStarted = false;
-        while((!ballLive && !turnStarted) ||//turn hasnt started and ball not in play
-                (turnStarted && ballLive))//ball in play, turn started
+        while(true)
         {
           readPlayfield();
-          if(ballLive){turnStarted = true;}
+          if(bally.getSwitch(0,7))
+          {
+            Serial.println("turn over");
+            bally.playSound(12);
+            ballLive = false;
+            break;
+          }
         }
         bally.playSound(20);
         if(activePlayer < numPlayers)//advance current player and/or ball number
@@ -64,40 +67,159 @@ void loop() {
       //gameOver(numPlayers);
 }
 
+//TODO bonus for doing rollover lane sets
 void readPlayfield()
 {
+  static unsigned char targetRow = 0;
   long int addScore = 0;
-  Serial.println("reading playfield");
-  if(bally.getSwitch(0,7))
-  {
-    Serial.println("Ball died");
-    ballLive = false;
-  }
-  else if(bally.getRedge(4, 2) || bally.getRedge(4, 3))
-  {
-    Serial.println("ball ring");
-    addScore += 20;
-  }
-  else if(bally.getRedgeRow(2) > 0)
-  {
-    Serial.println("target hit");
-    static int targetCounter = 0;
-    targetCounter++;
-    addScore += 10;
-    if(targetCounter >= 8)
-    {
-      bally.fireSolenoid(3, true, true);
-      bally.fireSolenoid(7, true, true); 
-      targetCounter = 0; 
-      addScore += 100;
-    }
-  }
+  //Serial.println("reading playfield");
   //read each playfield switch
   //for each switch hit, take appropriate action (add player,
   //fire solenoid, add points, play chime, arm bonus, etc.)
+  
+  if(bally.getRedge(4, 2) || bally.getRedge(4, 3))
+  {
+    Serial.println("ball ring");
+    bally.playSound(12);
+    addScore += 20;
+  }
+  else if(bally.getRedge(3, 0) || bally.getRedge(3, 1))
+  {
+    Serial.println("left/right feed lane");
+    bally.playSound(12);
+    addScore += 10;
+  }
+  else if(bally.getRedge(3, 2) && targetRow == 11111111)
+  {
+    bool allDown = true;
+    Serial.println("drop target rebound hit, checking bits");
+    for(int i = 0; i < 8; ++i)
+    {
+      if(bitRead(targetRow, i) == 0){allDown = false;}
+    }
+    if(allDown)
+    {
+      Serial.println("bits valid");
+      bally.playSound(12);
+      bally.fireSolenoid(3, true, true);
+      bally.fireSolenoid(7, true, true); 
+      targetRow = 0;
+      addScore += 100;
+    }
+  }
+  static int aHit = false;
+  static int bHit = false;
+  static int topA = false;
+  static int topB = false;
+  if(bally.getRedge(3, 3))
+  {
+    Serial.println("lane B");
+    bally.playSound(12);
+    bHit = true;
+    addScore += 20;
+  }
+  else if(bally.getRedge(3, 4))
+  {
+    Serial.println("lane A");
+    bally.playSound(12);
+    aHit = true;
+    addScore += 20;
+  }
+  else if(bally.getRedge(3, 5))
+  {
+    Serial.println("top B");
+    bally.playSound(12);
+    topB = true;
+    addScore += 20;
+  }
+  else if(bally.getRedge(3, 6))
+  {
+    Serial.println("top A");
+    bally.playSound(12);
+    topA = true;
+    addScore += 20;
+  }
+  else if(bally.getRedge(3, 7))
+  {
+    Serial.println("Top Center Kick");
+    bally.playSound(13);
+    bally.fireSolenoid(0, false);
+    addScore += 300;
+  }
+  else if(bally.getRedge(4, 0) || bally.getRedge(4, 1))
+  {
+    Serial.println("Out lane");
+    bally.playSound(12);
+    addScore += 10;
+  }
+  else if(bally.getRedge(4, 2))
+  {
+    Serial.println("Right Slingshot");
+    bally.playSound(12);
+    bally.fireSolenoid(11, false);
+    addScore += 10;
+  }
+  else if(bally.getRedge(4, 3))
+  {
+    Serial.println("Left Slingshot");
+    bally.playSound(12);
+    bally.fireSolenoid(13, false);
+    addScore += 10;
+  }
+  else if(bally.getRedge(4, 4))
+  {
+    Serial.println("Bottom Right Pop Bumper");
+    bally.playSound(12);
+    bally.fireSolenoid(5, false);
+    addScore += 10;
+  }
+  else if(bally.getRedge(4, 5))
+  {
+    Serial.println("Bottom Left Pop Bumper");
+    bally.playSound(12);
+    bally.fireSolenoid(1, false);
+    addScore += 10;
+  }
+  else if(bally.getRedge(4, 6))
+  {
+    Serial.println("Top Right Pop Bumper");
+    bally.playSound(12);
+    bally.fireSolenoid(9, false);
+    addScore += 10;
+  }
+  else if(bally.getRedge(4, 7))
+  {
+    Serial.println("Top Left Pop Bumper");
+    bally.playSound(12);
+    bally.fireSolenoid(1, false);
+    addScore += 10;
+  }//*/
+  
+  unsigned char currTarget = bally.getRedgeRow(2);
+  if(currTarget > 0)
+  {
+    Serial.println("target hit");
+    bally.playSound(12);
+    targetRow = targetRow | currTarget;    
+    addScore += 10;
+  }
+  //Apply Bonuses
+  if(aHit && bHit)
+  {
+    bHit = false;
+    aHit = false;
+    addScore += 200;
+  }
+  if(topA && topB)
+  {
+    topA = false;
+    topB = false;
+    addScore += 200;
+  }
   if(addScore > 0)
   {
     ballLive = true;
+    bally.setLamp(12, 0, true);;//ball in play
     scores[activePlayer] = scores[activePlayer] + addScore;
     updateScore(activePlayer, scores[activePlayer]);
   }
@@ -116,7 +238,7 @@ void switchPlayer(const int& playerIndex)//switch activePlayer logic
 void gameOver(int numPlayers)
 {
   lightLamp(12, 2);//turn on game over light
-  for(int i = 0; i < 4; i++){setScore(i, 0);}//set scores back to 0
+  for(int i = 0; i < 4; i++){turnOffDisplay(i);}//set scores back to 0
   activePlayer = -1;
   numPlayers =-1;
 }
@@ -124,6 +246,8 @@ void gameOver(int numPlayers)
 //==================================================finalized functions==========
 void initScreen()
 {
+  bally.fireSolenoid(3, true, true);//targets reset
+  bally.fireSolenoid(7, true, true); //""
   bally.setLamp(12, 2, true);//game over
   bally.setLamp(12, 0, false);//ball in play
   bally.setDisplay(4, 0, 0);//turn/ball num
@@ -132,10 +256,8 @@ void initScreen()
   bally.setDisplay(4, 3, 0);//credits
   bally.setDisplay(4, 4, -1);
   bally.setDisplay(4, 5, -1);
-  for(int i = 0; i < 4; i++){updateScore(i, scores[i]);}
-
-  bally.fireSolenoid(3, true, true);//targets reset
-  bally.fireSolenoid(7, true, true); 
+  //for(int i = 0; i < 4; i++){updateScore(i, scores[i]);}
+  for(int i = 0; i < 4; ++i){turnOffDisplay(i);}
   bally.zeroSwitchMemory();
 }
 
@@ -154,11 +276,12 @@ void waitPlayers(int& numPlayers, int& credits)
     result = addPlayerReleased();
     if(result){
       if(credits > 0 && numPlayers < 3){
-        Serial.print("Add Player ");
-        Serial.println(numPlayers);
         credits--;
+        bally.playSound(6);
         bally.setDisplay(4, 3, credits);
         numPlayers++;
+        Serial.print("Add Player ");
+        Serial.println(numPlayers);
         lightLamp(NUM_PLAYERS_ROW, numPlayers);
         if(numPlayers == 0)
         {
@@ -167,9 +290,10 @@ void waitPlayers(int& numPlayers, int& credits)
           bally.setContSolenoid(2, false);
           delay(20);
         }
+        else{updateScore(numPlayers, scores[numPlayers]);}
       }
     }
-    readPlayfield();
+    if(numPlayers >= 0){readPlayfield();}//cant start game until there is a player
   }
   Serial.println("Game Started");
 }
@@ -223,20 +347,40 @@ void updateScore(int dispNumber, int newScore)
   Serial.print("setScore ");
   Serial.println(newScore);
   long int scoreCpy = newScore;
+  int temp[7] = {0};
   for(int i = 0; i < N_DIGITS; ++i)
     {
-      int temp = removeSmallestDigit(scoreCpy);
-      if(i != 0 &&temp == 0)
+      temp[i] = removeSmallestDigit(scoreCpy);
+      /*if(!bitFound)
       {
-        temp = -1;
-      }
-      bally.setDisplay(dispNumber, i, temp);
+        if(temp != 0){bitFound = true;}
+        else{temp = -1;}
+      }//*/
+      //bally.setDisplay(dispNumber, i, temp);
     } 
+  bool bitFound = false;
+  int counter = N_DIGITS - 1;
+  while(true)//turn off leading digits
+  {
+      if(!bitFound && temp[counter] == 0){temp[counter] = -1;}
+      else{bitFound = true;}
+      counter--;
+      bally.setDisplay(dispNumber, counter, temp[counter]);
+      if(counter < 0){break;}
+  }
+}
+
+void turnOffDisplay(int dispNumber)
+{
+  for(int i = 0; i < N_DIGITS; ++i)
+  {
+    bally.setDisplay(dispNumber, i, 11);
+  }
 }
 
 int removeSmallestDigit(long int& score)//takes score splits off leading bit from original score
 {
-  int result = (int)(score % 10);
+  long int result = (long int)(score % 10);
   score = score / 10;
-  return result;
+  return (int)result;
 }
